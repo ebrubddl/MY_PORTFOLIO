13ab4ccd7fe40a87f3f18d22045eea8278017ee8
Ebru Baddal <98316380+ebrubddl@users.noreply.github.com>
2022-02-02 17:34:23 +0300

    Create Caesar cipher.py
    Would you like to encrypt your deepest secrets?

---
+++ Caesar cipher.py
@@ -0,0 +1,45 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(textt, shift_amount, cipher_direction):
+  cyper_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for character in textt:
+    if character in alphabet:
+      position = alphabet.index(character)
+      new_position = position + shift_amount
+      cyper_text += alphabet[new_position]
+    else:
+      cyper_text += character
+  print(f"Here's the {cipher_direction}d result: {cyper_text}")
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+should_end = False
+while not should_end:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  #What if the user enters a shift amount that is greater than the number of letters in the alphabet?
+  shift = shift % 26
+  caesar(textt=text, shift_amount=shift, cipher_direction=direction)
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("See you soon...")
