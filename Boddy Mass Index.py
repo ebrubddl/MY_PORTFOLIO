5e7836afce4c598e717d3b9ffe2d10d41b7885ef
Ebru Baddal <98316380+ebrubddl@users.noreply.github.com>
2022-05-07 17:39:26 +0300

    Update Boddy Mass Index.py

--- Boddy Mass Index.py
+++ Boddy Mass Index.py
@@ -5,4 +5,13 @@ BodyMassIndex=weight/height**2
 
 BMI=round(BodyMassIndex)
 
-print(f"Your BMI is {BMI}.")
+if BMI<18.5:
+    print(f"Your BMI is {BMI} and you're underweight")
+elif BMI<25:
+    print(f"Your BMI is {BMI} and you're normal weight")
+elif BMI<30:
+    print(f"Your BMI is {BMI} and you're slightly weight")
+elif BMI<35:
+    print(f"Your BMI is {BMI} and you're obese")
+else:
+    print(f"Your BMI is {BMI} and you're clinically obese")
